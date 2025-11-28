baad0c5f2e15c16d764e8ecb85fabacfa8f9c6a2
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2025-11-28 23:19:18 +0200

    Remove comments

--- echo.c
+++ echo.c
@@ -13,14 +13,9 @@
 #define BUF_SIZE 1024
 #define MAX_CLIENTS 512
 
-static Stack *g_stack = NULL;
-
-/* Poll arrays */
 static WSAPOLLFD pollfds[MAX_CLIENTS + 1];
 static Coroutine *coros[MAX_CLIENTS + 1];
-static int poll_count = 0; /* includes server socket at index 0 */
-
-/* ----------------------------------------------------------- */
+static int poll_count = 0;
 
 static void PrintLastError(const char *msg) {
     DWORD id = GetLastError();
@@ -35,9 +30,6 @@ static void PrintLastError(const char *msg) {
     LocalFree(buf);
 }
 
-/* -----------------------------------------------------------
-   CLIENT COROUTINE
-   ----------------------------------------------------------- */
 static void client_coroutine(Stack *stack, void *arg) {
     SOCKET s = (SOCKET)(intptr_t)arg;
     char buf[BUF_SIZE];
@@ -54,15 +46,12 @@ static void client_coroutine(Stack *stack, void *arg) {
             break;
         }
 
-        /* allow scheduler to resume others */
         coroutine_yield(stack);
     }
 
     closesocket(s);
 }
-/* -----------------------------------------------------------
-   ACCEPT COROUTINE
-   ----------------------------------------------------------- */
+
 static void accept_coroutine(Stack *stack, void *arg) {
     SOCKET serv = (SOCKET)(intptr_t)arg;
 
@@ -77,43 +66,22 @@ static void accept_coroutine(Stack *stack, void *arg) {
 			if (poll_count < MAX_CLIENTS + 1) {
 				pollfds[poll_count].fd = c;
 				pollfds[poll_count].events = POLLRDNORM;
-				coros[poll_count++] = coroutine_create(g_stack, client_coroutine, (void*)(intptr_t)c);
+				coros[poll_count++] = coroutine_create(stack, client_coroutine, (void*)(intptr_t)c);
 			} else {
 				closesocket(c);
 			} 
         }
 
-        /* yield back to scheduler */
         coroutine_yield(stack);
     }
 }
-/* -----------------------------------------------------------
-   REMOVE CLIENT ENTRY
-   ----------------------------------------------------------- */
-static void remove_client(int index) {
-    /* index is in pollfds/coros */
 
-    /* destroy coroutine */
-    coroutine_destroy(coros[index]);
-
-    /* remove socket from poll array */
-    pollfds[index] = pollfds[poll_count - 1];
-    coros[index]    = coros[poll_count - 1];
-
-    poll_count--;
-}
-
-/* -----------------------------------------------------------
-   MAIN
-   ----------------------------------------------------------- */
 int main(void) {
-    /* winsock */
     {
         WSADATA w;
         assert(WSAStartup(MAKEWORD(2,2), &w) == 0);
     }
 
-    /* server socket */
     SOCKET serv = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     assert(serv != INVALID_SOCKET);
 
@@ -129,34 +97,32 @@ int main(void) {
         u_long mode = 1;
         ioctlsocket(serv, FIONBIO, &mode);
     }
-    /* coroutine stack */
-    g_stack = coroutine_init();
-    assert(g_stack);
+    Stack* stack = coroutine_init();
+    assert(stack);
 
-    /* setup poll list */
     pollfds[0].fd = serv;
     pollfds[0].events = POLLRDNORM;
-    coros[0] = NULL; /* server has no coroutine */
-    poll_count = 0;
+	coros[0] = coroutine_create(stack, accept_coroutine, (void*)(intptr_t)serv);
+    poll_count = 1;
 
-	coros[poll_count++] = coroutine_create(g_stack, accept_coroutine, (void*)(intptr_t)serv);
-    /* event loop */
     for (;;) {
         int r = WSAPoll(pollfds, poll_count, -1);
         if (r == SOCKET_ERROR) {
             PrintLastError("WSAPoll");
             continue;
         }
-        /* CLIENTS READY */
         int i = 0;
         while (i < poll_count) {
             if (pollfds[i].revents & POLLRDNORM) {
                 Coroutine *co = coros[i];
-                coroutine_resume(g_stack, co);
+                coroutine_resume(stack, co);
 
                 if (co->finished) {
-                    remove_client(i);
-                    continue; /* do not increment i (array shifted) */
+				coroutine_destroy(coros[i]);
+				pollfds[i] = pollfds[poll_count - 1];
+				coros[i]    = coros[poll_count - 1];
+				poll_count--;              
+				continue; /* do not increment i (array shifted) */
                 }
             }
             i++;
