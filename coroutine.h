e0669afe3cd69db08bcdad5f35d4e9223520da75
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2025-11-28 23:24:10 +0200

    Format everything

--- coroutine.h
+++ coroutine.h
@@ -5,25 +5,25 @@
 #include <stdbool.h>
 
 typedef struct {
-    void *rsp;
-    void *stack_base;
-    bool finished;
+	void* rsp;
+	void* stack_base;
+	bool finished;
 } Coroutine;
 
 // TODO: make the STACK_CAPACITY customizable by the user
-#define STACK_CAPACITY (4*1024*1024)
+#define STACK_CAPACITY (4 * 1024 * 1024)
 
 typedef struct {
-    Coroutine **items;
-    size_t count;
-    size_t capacity;
+	Coroutine** items;
+	size_t count;
+	size_t capacity;
 } Stack;
 
-Stack *coroutine_init(void);
-void coroutine_deinit(Stack *stack);
-Coroutine *coroutine_create(Stack *stack, void (*f)(Stack*, void*), void *arg);
-void coroutine_destroy(Coroutine *c);
-void coroutine_resume(Stack *stack, Coroutine *c);
-void coroutine_yield(Stack *stack);
+Stack* coroutine_init(void);
+void coroutine_deinit(Stack* stack);
+Coroutine* coroutine_create(Stack* stack, void (*f)(Stack*, void*), void* arg);
+void coroutine_destroy(Coroutine* c);
+void coroutine_resume(Stack* stack, Coroutine* c);
+void coroutine_yield(Stack* stack);
 
 #endif // COROUTINE_PANIM_H_
--- echo.c
+++ echo.c
@@ -14,48 +14,44 @@
 #define MAX_CLIENTS 512
 
 static WSAPOLLFD pollfds[MAX_CLIENTS + 1];
-static Coroutine *coros[MAX_CLIENTS + 1];
+static Coroutine* coros[MAX_CLIENTS + 1];
 static int poll_count = 0;
 
-static void PrintLastError(const char *msg) {
-    DWORD id = GetLastError();
-    LPSTR buf = NULL;
-    FormatMessageA(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL, id,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPSTR)&buf, 0, NULL
-    );
-    printf("%s: %s\n", msg, buf);
-    LocalFree(buf);
+static void PrintLastError(const char* msg) {
+	DWORD id = GetLastError();
+	LPSTR buf = NULL;
+	FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL,
+				   id, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buf, 0, NULL);
+	printf("%s: %s\n", msg, buf);
+	LocalFree(buf);
 }
 
-static void client_coroutine(Stack *stack, void *arg) {
-    SOCKET s = (SOCKET)(intptr_t)arg;
-    char buf[BUF_SIZE];
+static void client_coroutine(Stack* stack, void* arg) {
+	SOCKET s = (SOCKET)(intptr_t)arg;
+	char buf[BUF_SIZE];
 
-    for (;;) {
-        int r = recv(s, buf, sizeof(buf), 0);
-        if (r > 0) {
-            send(s, buf, r, 0);
-        } else if (r == 0) {
-            /* disconnect */
-            break;
-        } else {
-            PrintLastError("recv");
-            break;
-        }
+	for (;;) {
+		int r = recv(s, buf, sizeof(buf), 0);
+		if (r > 0) {
+			send(s, buf, r, 0);
+		} else if (r == 0) {
+			/* disconnect */
+			break;
+		} else {
+			PrintLastError("recv");
+			break;
+		}
 
-        coroutine_yield(stack);
-    }
+		coroutine_yield(stack);
+	}
 
-    closesocket(s);
+	closesocket(s);
 }
 
-static void accept_coroutine(Stack *stack, void *arg) {
-    SOCKET serv = (SOCKET)(intptr_t)arg;
+static void accept_coroutine(Stack* stack, void* arg) {
+	SOCKET serv = (SOCKET)(intptr_t)arg;
 
-    for (;;) {
+	for (;;) {
 		SOCKET c = accept(serv, NULL, NULL);
 		if (c == INVALID_SOCKET) {
 			PrintLastError("accept");
@@ -69,65 +65,65 @@ static void accept_coroutine(Stack *stack, void *arg) {
 				coros[poll_count++] = coroutine_create(stack, client_coroutine, (void*)(intptr_t)c);
 			} else {
 				closesocket(c);
-			} 
-        }
+			}
+		}
 
-        coroutine_yield(stack);
-    }
+		coroutine_yield(stack);
+	}
 }
 
 int main(void) {
-    {
-        WSADATA w;
-        assert(WSAStartup(MAKEWORD(2,2), &w) == 0);
-    }
+	{
+		WSADATA w;
+		assert(WSAStartup(MAKEWORD(2, 2), &w) == 0);
+	}
 
-    SOCKET serv = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    assert(serv != INVALID_SOCKET);
+	SOCKET serv = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	assert(serv != INVALID_SOCKET);
 
-    {
-        struct sockaddr_in a;
-        a.sin_family = AF_INET;
-        a.sin_addr.s_addr = INADDR_ANY;
-        a.sin_port = htons(PORT);
+	{
+		struct sockaddr_in a;
+		a.sin_family = AF_INET;
+		a.sin_addr.s_addr = INADDR_ANY;
+		a.sin_port = htons(PORT);
 
-        assert(bind(serv, (struct sockaddr*)&a, sizeof(a)) != SOCKET_ERROR);
-        assert(listen(serv, SOMAXCONN) != SOCKET_ERROR);
+		assert(bind(serv, (struct sockaddr*)&a, sizeof(a)) != SOCKET_ERROR);
+		assert(listen(serv, SOMAXCONN) != SOCKET_ERROR);
 
-        u_long mode = 1;
-        ioctlsocket(serv, FIONBIO, &mode);
-    }
-    Stack* stack = coroutine_init();
-    assert(stack);
+		u_long mode = 1;
+		ioctlsocket(serv, FIONBIO, &mode);
+	}
+	Stack* stack = coroutine_init();
+	assert(stack);
 
-    pollfds[0].fd = serv;
-    pollfds[0].events = POLLRDNORM;
+	pollfds[0].fd = serv;
+	pollfds[0].events = POLLRDNORM;
 	coros[0] = coroutine_create(stack, accept_coroutine, (void*)(intptr_t)serv);
-    poll_count = 1;
+	poll_count = 1;
 
-    for (;;) {
-        int r = WSAPoll(pollfds, poll_count, -1);
-        if (r == SOCKET_ERROR) {
-            PrintLastError("WSAPoll");
-            continue;
-        }
-        int i = 0;
-        while (i < poll_count) {
-            if (pollfds[i].revents & POLLRDNORM) {
-                Coroutine *co = coros[i];
-                coroutine_resume(stack, co);
+	for (;;) {
+		int r = WSAPoll(pollfds, poll_count, -1);
+		if (r == SOCKET_ERROR) {
+			PrintLastError("WSAPoll");
+			continue;
+		}
+		int i = 0;
+		while (i < poll_count) {
+			if (pollfds[i].revents & POLLRDNORM) {
+				Coroutine* co = coros[i];
+				coroutine_resume(stack, co);
 
-                if (co->finished) {
-				coroutine_destroy(coros[i]);
-				pollfds[i] = pollfds[poll_count - 1];
-				coros[i]    = coros[poll_count - 1];
-				poll_count--;              
-				continue; /* do not increment i (array shifted) */
-                }
-            }
-            i++;
-        }
-    }
+				if (co->finished) {
+					coroutine_destroy(coros[i]);
+					pollfds[i] = pollfds[poll_count - 1];
+					coros[i] = coros[poll_count - 1];
+					poll_count--;
+					continue; /* do not increment i (array shifted) */
+				}
+			}
+			i++;
+		}
+	}
 
-    return 0;
+	return 0;
 }
